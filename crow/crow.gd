c12e026b532eb8fd0855671e07dfb1a8ef99c5c2
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-10-16 10:56:20 -0400

    feat: flip crow to match movement direction

--- crow/crow.gd
+++ crow/crow.gd
@@ -1,10 +1,31 @@
 extends CharacterBody2D
 
+## Constants
 static var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 const GROUND_SPEED = 500
 const FLIGHT_SPEED = 1000
+@onready var INITIAL_SCALE = $BodyCollider.scale.x
 
+
+## Onready Variables
 @onready var Sprite = $Sprite
+@onready var BodySprite = $Sprite/BodySprite
+@onready var EyeSprite = $Sprite/BodySprite/EyeSprite
+@onready var WingSprite = $Sprite/WingSprite
+@onready var BodyCollider = $BodyCollider
+
+
+## Complex Variables
+var flipped: bool :
+	set(value):
+		flipped = value
+		Sprite.flip_h = value
+		BodySprite.flip_h = value
+		EyeSprite.flip_h = value
+		WingSprite.flip_h = value
+		BodyCollider.scale.x = -INITIAL_SCALE if value else INITIAL_SCALE
+
+
 
 func _physics_process(delta): 
 	var speed = GROUND_SPEED if is_on_floor() else FLIGHT_SPEED
@@ -13,10 +34,10 @@ func _physics_process(delta):
 	## Movement direction
 	if Input.is_action_pressed("MoveLeft"): 
 		direction = -1
-		Sprite.flip_h = true
+		self.flipped = true
 	if Input.is_action_pressed("MoveRight"): 
 		direction = 1
-		Sprite.flip_h = false
+		self.flipped = false
 	velocity.x = direction * speed
 	
 	## Fly
