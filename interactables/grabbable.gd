1e19f863b1dd0859ad858050961d05856249eab8
emilierummer <114100983+emilierummer@users.noreply.github.com>
2024-10-17 23:49:39 -0400

    feat: house outline

--- interactables/grabbable.gd
+++ interactables/grabbable.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 class_name Grabbable
 
+signal picked_up
+signal dropped
+
 static var GRAVITY = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var velocity = Vector2(0, 0)
@@ -15,10 +18,12 @@ var held_by: Node2D :
 func pick_up(picked_up_by):
 	held_by = picked_up_by
 	rotation = 0
+	picked_up.emit()
 
 
 func drop():
 	held_by = null
+	dropped.emit()
 
 
 func _physics_process(_delta):
